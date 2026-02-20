25581d937b553d0a449410aa70f9b2533f2ac54a
adiauti <autiaditya2007@gmail.com>
2026-02-21 00:25:45 +0530

    Fixed a Start error

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 let startime;
 let endtime;
 let gamestate;
+let clr1;
+let clr2;
+
+function startgame() {
+    document.getElementById("gameArea").innerHTML= "<p style='font-size:40px'><b>Wait for green!</b></p>";
+    gameArea.style.backgroundColor = "rgb(253,100,100)";
+    gamestate = "waiting!";
+    clr1 = setTimeout(changecolor,5000);
+}
 
 function changecolor() {
-    document.getElementById("gameArea").style.backgroundColor = rgb(69, 232, 88);
+    document.getElementById("gameArea").style.backgroundColor = "rgb(69, 232, 88)";
     startime=Date.now();
     gamestate = "ready!";
-}
-function startgame() {
-    clr = setTimeout(changecolor,5000);
-    gameArea.style.backgroundColor = rgb(253,100,100);
-    document.getElementById("gameArea").innerHTML= "<p><b>Wait for green!</b></p>";
-    gamestate = "waiting!";
 }
 
 gameArea.addEventListener("click", function() {
     if (gamestate === "waiting!") {
-        document.getElementById("gameArea").innerHTML = "<p><b>Too soon ! Wait for green!</b></p>";
-        clearTimeout(clr);
+        document.getElementById("gameArea").innerHTML = "<p style='font-size:40px'><b>Too soon ! Wait for green!</b></p>";
+        clearTimeout(clr1);
         clr2 = setTimeout(startgame, 2000);
     }
     else if (gamestate === "ready!") {
         endtime = Date.now();
         reactiontime = (endtime - startime);
-        document.getElementById("gameArea").innerHTML = "<p><b>"+reactiontime+"ms</b></p>";
+        document.getElementById("gameArea").innerHTML = "<p style='font-size:40px'><b>"+reactiontime+"ms</b></p>";
         gamestate="Finished!";
-        clearTimeout(clr);   
+        clearTimeout(clr1);   
     }
     else{
-        document.getElementById("gameArea").innerHTML = "<p><b>Click start to play again!</b></p>";
+        document.getElementById("gameArea").innerHTML = "<p style='font-size:40px'><b>Click start to play again!</b></p>";
         return;
     }
 });
